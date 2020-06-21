36dc13f1a5e21784aedc03356f19c81fd877aaf0
edmayen <ed_mayen@hotmail.com>
2020-06-21 16:09:49 -0500

    Firs commit

---
+++ app/src/main/java/com/example/firebaseauth/MainActivity.kt
@@ -0,0 +1,89 @@
+package com.example.firebaseauth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        auth = FirebaseAuth.getInstance()
+        auth.signOut()
+        btnRegister.setOnClickListener {
+            registerUser()
+        }
+
+        btnLogin.setOnClickListener {
+            loginUser()
+        }
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        checkLoggedInState()
+    }
+
+    private fun registerUser(){
+        val email = etEmailRegister.text.toString()
+        val password = etPasswordRegister.text.toString()
+        if(email.isNotEmpty() && password.isNotEmpty()){
+            CoroutineScope(Dispatchers.IO).launch{
+                try {
+                    auth.createUserWithEmailAndPassword(email, password)
+                    withContext(Dispatchers.Main){
+                        checkLoggedInState()
+                    }
+                }catch (e: Exception){
+                    withContext(Dispatchers.Main){
+                        Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_LONG).show()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun loginUser(){
+        val email = etEmailLogin.text.toString()
+        val password = etPasswordLogin.text.toString()
+        if(email.isNotEmpty() && password.isNotEmpty()){
+            CoroutineScope(Dispatchers.IO).launch{
+                try {
+                    auth.signInWithEmailAndPassword(email, password)
+                    withContext(Dispatchers.Main){
+                        checkLoggedInState()
+                    }
+                }catch (e: Exception){
+                    withContext(Dispatchers.Main){
+                        Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_LONG).show()
+                    }
+                }
+            }
+        }
+    }
+
+
+    private fun checkLoggedInState(){
+        if(auth.currentUser == null){
+            tvLoggedIn.text = "You are not logged in"
+        }
+        else{
+            tvLoggedIn.text = "You are logged in"
+        }
+    }
+
+
+
+
+
+}
